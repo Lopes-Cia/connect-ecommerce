7bff36dd8669c6edc6678e006fccc43a47961152
eduardorezende-ops <eduardo.rezende@lopesecia.com.br>
2026-04-13 17:04:10 -0300

    add ambev permissao

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,17 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "gp.lopesecia.com.br",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "lopesecia.com.br",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.catalogoambev.com.br",
+        pathname: "/**",
       },
     ],
   },
